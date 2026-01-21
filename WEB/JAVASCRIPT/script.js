cbc843f7cf0cbd2399935129584ab147be67fa68
nancysgtic-star <nancy.sg.tic@gmail.com>
2026-01-21 09:29:56 -0600

    Estructura inicial del proyecto web

---
+++ WEB/JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+//obtener el boton por su ID que se puso 
+const boton1=document.getElementById('tilin');
+
+// Agregar un elemento al boton
+boton1.addEventListener('click' ,function(){
+	alert("YEAHH");
+});
