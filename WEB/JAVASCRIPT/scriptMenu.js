edb2cd5b8a83e7b4f658a3e916c193f52156d024
nancysgtic-star <nancy.sg.tic@gmail.com>
2026-01-29 09:38:20 -0600

    Add files via upload

--- WEB/JAVASCRIPT/scriptMenu.js
+++ WEB/JAVASCRIPT/scriptMenu.js
@@ -1,10 +1,3 @@
-//obtener el boton por su ID que se puso 
-const boton1=document.getElementById('tilin');
-
-// Agregar un elemento al boton
-boton1.addEventListener('click' ,function(){
-	alert("YEAHH");
-});
 function toggleMobileMenu() {
     const menu = document.getElementById("mobileMenu");
     if (menu.style.width === "100%") {
